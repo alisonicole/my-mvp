b7894f9c6e58f156c690bacdd86737cec183845f
Alison Lee <lee.alisonnicole@gmail.com>
2026-03-11 21:39:17 -0400

    fix login and signup pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./AppRouter";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import "./index.css";
 
 class ErrorBoundary extends React.Component {
@@ -29,7 +31,11 @@ class ErrorBoundary extends React.Component {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
     <BrowserRouter>
-      <AppRouter />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </ErrorBoundary>
 );
